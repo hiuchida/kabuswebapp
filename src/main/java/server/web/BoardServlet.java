fd6b751226e04975167da536da8ee08c3d79a9bb
hiuchida <hiro4uchida@gmail.com>
2022-10-21 13:05:21 +0900

    時価情報(/board)

---
+++ src/main/java/server/web/BoardServlet.java
@@ -0,0 +1,53 @@
+package server.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import io.swagger.client.ApiException;
+import io.swagger.client.api.InfoApi;
+import io.swagger.client.model.BoardSuccess;
+import logic.LockedAuthorizedTokenLogic;
+
+/**
+ * Servlet implementation class BoardServlet
+ */
+public class BoardServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		try (LockedAuthorizedTokenLogic login = new LockedAuthorizedTokenLogic()) {
+			String X_API_KEY = login.getApiKey();
+	        String symbol = "9433@1"; // ＫＤＤＩ
+	        InfoApi infoApi = new InfoApi();
+	        BoardSuccess board = infoApi.boardGet(X_API_KEY, symbol);
+	        System.out.println(board);
+			response.getWriter().append(board.toString());
+		} catch (ApiException e) {
+			e.printStackTrace();
+			response.getWriter().append("Error: ").append(e.getResponseBody());
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}

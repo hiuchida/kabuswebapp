bd80d46d392a1b3b7e6b967ea7ad3e2f44f90c42
hiuchida <hiro4uchida@gmail.com>
2022-10-31 03:41:33 +0900

    switch kabuslib.jar

--- src/test/java/io/swagger/client/api/TestConsts.java
+++
@@ -1,9 +0,0 @@
-package io.swagger.client.api;
-
-public class TestConsts {
-
-	public static final String API_PASSWORD = "YourPassword";
-
-	public static final String TRADE_PASSWORD = "YourTradePassword";
-
-}
